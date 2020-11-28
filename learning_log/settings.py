668af663e96cad3703349b7f5b705a64df40744f
amyjprogrammer <70500009+amyjprogrammer@users.noreply.github.com>
2020-11-28 16:32:02 -0500

    Security for my secret key

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oz@z#aane7o^w4remm=a8y_ei3_ha%nj7u*(+@19c9u9@8nq_d'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,3 +139,6 @@ if os.environ.get('DEBUG')== 'TRUE':
     DEBUG = True
 if os.environ.get('DEBUG')== 'FALSE':
     DEBUG = False
+
+from dotenv import load_dotenv
+load_dotenv()
